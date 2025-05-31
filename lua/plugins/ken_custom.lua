7075eb91b6c4a296861f7eeb3b7f76bbc189dd15
Ken M <ken@mack-z.com>
2025-05-31 14:46:12 +0000

    initial migration from v4

---
+++ lua/plugins/ken_custom.lua
@@ -0,0 +1,68 @@
+return {
+  { "tpope/vim-surround", lazy = false },
+  -- {
+  --   "EdenEast/nightfox.nvim",
+  --   lazy = false,
+  -- },
+  {
+    "klen/nvim-test",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-test").setup {
+        run = true, -- run tests (using for debug)
+        commands_create = true, -- create commands (TestFile, TestLast, ...)
+        filename_modifier = ":p:.", -- modify filenames before tests run(:h filename-modifiers)
+        silent = false, -- less notifications
+        term = "terminal", -- a terminal to run ("terminal"|"toggleterm")
+        termOpts = {
+          direction = "vertical", -- terminal's direction ("horizontal"|"vertical"|"float")
+          width = 128, -- terminal's width (for vertical|float)
+          height = 24, -- terminal's height (for horizontal|float)
+          go_back = false, -- return focus to original window after executing
+          stopinsert = "auto", -- exit from insert mode (true|false|"auto")
+          keep_one = true, -- keep only one terminal for testing
+        },
+        runners = {
+          -- setup tests runners
+          cs = "nvim-test.runners.dotnet",
+          go = "nvim-test.runners.go-test",
+          haskell = "nvim-test.runners.hspec",
+          javascriptreact = "nvim-test.runners.jest",
+          javascript = "nvim-test.runners.jest",
+          lua = "nvim-test.runners.busted",
+          python = "nvim-test.runners.pytest",
+          ruby = "nvim-test.runners.rspec",
+          rust = "nvim-test.runners.cargo-test",
+          typescript = "nvim-test.runners.jest",
+          typescriptreact = "nvim-test.runners.jest",
+        },
+      }
+    end,
+  },
+  {
+    "mrjones2014/smart-splits.nvim",
+    event = "VeryLazy",
+    config = function()
+      vim.keymap.set("n", "<C-[>", require("smart-splits").resize_left)
+      vim.keymap.set("n", "<C-;>", require("smart-splits").resize_down)
+      vim.keymap.set("n", "<C-p>", require("smart-splits").resize_up)
+      vim.keymap.set("n", "<C-]>", require("smart-splits").resize_right)
+    end,
+  },
+  -- {
+  --   "jackMort/ChatGPT.nvim",
+  --   event = "VeryLazy",
+  --   config = function() require("chatgpt").setup() end,
+  --   dependencies = {
+  --     "MunifTanjim/nui.nvim",
+  --     "nvim-lua/plenary.nvim",
+  --     "nvim-telescope/telescope.nvim",
+  --   },
+  -- },
+  {
+    "akinsho/toggleterm.nvim",
+    opts = {
+      shell = "/usr/bin/fish",
+    },
+  },
+}
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,9 +1,15 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- Customize Mason
 
 ---@type LazySpec
 return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      pip = {
+        upgrade_pip = true,
+      },
+    },
+  },
   -- use mason-tool-installer for automatically installing Mason packages
   {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
@@ -13,12 +19,34 @@ return {
       ensure_installed = {
         -- install language servers
         "lua-language-server",
+        "jedi-language-server",
+        "bash-language-server",
+        "eslint-lsp",
+        "remark-language-server",
+        "terraform-ls",
+        "tflint",
+        "yaml-language-server",
+        "typescript-language-server",
 
         -- install formatters
         "stylua",
+        "ruff",
+        "prettier",
+        "stylua",
+        "eslint_d",
+        "markdownlint",
+        "shellcheck",
+        "tflint",
+        "yamllint",
+        "markdownlint",
+        "prettier",
+        "prettierd",
 
         -- install debuggers
         "debugpy",
+        "bash-debug-adapter",
+        "debugpy",
+        "js-debug-adapter",
 
         -- install any other package
         "tree-sitter-cli",
