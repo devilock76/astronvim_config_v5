cdff663f7df1840eaaad2b9faf341151d5a9f221
Ken M <kmackenzie@cap-rx.com>
2025-07-18 22:08:15 +0000

    adding copilot + code companion

--- lua/plugins/ken_custom.lua
+++ lua/plugins/ken_custom.lua
@@ -65,4 +65,77 @@ return {
       shell = "/usr/bin/fish",
     },
   },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    event = "BufReadPost",
+    opts = {
+      panel = {
+        enabled = true,
+        auto_refresh = true,
+        keymap = {
+          jump_prev = "[[",
+          jump_next = "]]",
+          accept = "<CR>",
+          refresh = "gr",
+          open = "<M-CR>"
+        },
+        layout = {
+          position = "right", -- | top | left | right | horizontal | vertical
+          ratio = 0.4
+        },
+      },
+      suggestion = {
+        keymap = {
+          accept = false, -- handled by completion engine
+        },
+      },
+    },
+    specs = {
+      {
+        "AstroNvim/astrocore",
+        opts = {
+          options = {
+            g = {
+              -- set the ai_accept function
+              ai_accept = function()
+                if require("copilot.suggestion").is_visible() then
+                  require("copilot.suggestion").accept()
+                  return true
+                end
+              end,
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    "Saghen/blink.cmp",
+    optional = true,
+    opts = function(_, opts)
+      if not opts.keymap then
+        opts.keymap = {}
+      end
+      opts.keymap["<Tab>"] = {
+        "snippet_forward",
+        function()
+          if vim.g.ai_accept then
+            return vim.g.ai_accept()
+          end
+        end,
+        "fallback",
+      }
+      opts.keymap["<S-Tab>"] = { "snippet_backward", "fallback" }
+    end,
+  },
+  {
+    "olimorris/codecompanion.nvim",
+    opts = {},
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+  },
 }
